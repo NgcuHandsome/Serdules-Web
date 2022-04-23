6fdcc9276cd94aa7c79874fd2ad3ba86134c294c
nguyen ba cu <cuninjazas3@gmail.com>
2022-04-23 08:27:33 +0700

    update ui #3

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ lottie.loadAnimation({
     autoplay: true,
     path: './assets/json/slide3.json' // the path to the animation json
 });
+
+lottie.loadAnimation({
+    container: document.getElementById('slide4_ani'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './assets/json/slide4.json' // the path to the animation json
+});
+
+scrollId = {
+    'home_page_btn': '#slide_view',
+    'function_btn': '#function',
+    'contact_btn': 'contact'
+}
+
+$(".menu").click(function () {
+    let id = $(this).attr('id');
+    $('#main').animate({
+        scrollTop: $(scrollId[id]).offset().top
+    }, 300);
+});
